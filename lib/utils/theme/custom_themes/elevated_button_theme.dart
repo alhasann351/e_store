0cc258444b60ba5822f998618b9fc3ca44defba5
alhasann351 <alhasann352@gmail.com>
2024-04-03 23:01:17 +0600

    appbar-elevatedButton-text-theme-added

---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class EElevatedButtonTheme {
+  EElevatedButtonTheme._();
+
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledBackgroundColor: Colors.grey,
+      disabledForegroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      padding: const EdgeInsets.symmetric(vertical: 18.0),
+      textStyle: const TextStyle(
+          fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.white),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+    ),
+  );
+  static final darkElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledBackgroundColor: Colors.grey,
+      disabledForegroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      padding: const EdgeInsets.symmetric(vertical: 18.0),
+      textStyle: const TextStyle(
+          fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.white),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+    ),
+  );
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,26 @@
+import 'package:e_store/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:e_store/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class EAppTheme {
+  EAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: ETextTheme.lightTextTheme,
+    elevatedButtonTheme: EElevatedButtonTheme.lightElevatedButtonTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: ETextTheme.darkTextTheme,
+    elevatedButtonTheme: EElevatedButtonTheme.darkElevatedButtonTheme,
+  );
+}
