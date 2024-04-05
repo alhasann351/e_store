2f44280badb0e1067233e4b6688fe54fceb1a75d
alhasann351 <alhasann352@gmail.com>
2024-04-05 23:23:47 +0600

    create-colors-class

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:e_store/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: EAppTheme.lightTheme,
-      darkTheme: EAppTheme.darkTheme,
-    );
-  }
 }
---
+++ lib/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'utils/theme/theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: EAppTheme.lightTheme,
+      darkTheme: EAppTheme.darkTheme,
+    );
+  }
+}
