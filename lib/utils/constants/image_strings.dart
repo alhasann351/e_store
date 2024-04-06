dcbceb730ea0143f5d1db10e8b834307238016fa
alhasann351 <alhasann352@gmail.com>
2024-04-06 22:16:40 +0600

    create-imagestrings-textstrings-class

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+class EImages{
+  EImages._();
+
+  //app logo
+  static const String darkAppLogo = '';
+  static const String lightAppLogo = '';
+
+  //social logos
+  static const String google = '';
+  static const String facebook = '';
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,20 @@
+class ETexts {
+  ETexts._();
+
+  //global texts
+
+
+  //onboarding texts
+  static const String onBoardingTitle1 = 'Choose your product';
+  static const String onBoardingTitle2 = 'Select your payment method';
+  static const String onBoardingTitle3 = 'Deliver at your door step';
+
+  //onboarding subtitle texts
+  static const String onBoardingSubTitle1 = 'Welcome to a world of limitless choices - Your perfect product awaits!';
+  static const String onBoardingSubTitle2 = 'For seamless transactions, Choose your payment path - Your convenience, Our priority!';
+  static const String onBoardingSubTitle3 = 'From our doorstep to yours - Swift, Secure and Contactless delivery!';
+
+  //home text
+  static const String homeAppbarTitle = 'Good day for shopping';
+  static const String homeAppbarSubTitle = 'Al-Hasan';
+}
